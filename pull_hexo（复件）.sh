689592de239f6adb48c47e309dfc4c7c2b51afee
ansel <justap@163.com>
2021-04-25 13:49:46 +0800

    11

---
+++ pull_hexo（复件）.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cd /home/ansel/webservers
+git pull origin master
+
+cd /home/ansel/other
+git pull origin master
+
+cd /home/ansel/rk3399
+git pull origin master
+
+cd /home/ansel/STM32F103_HAL
+git pull origin master
